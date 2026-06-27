59216a3f1cce44089ca1308226e4ab90e7e8c6a5
Hawkyy <tr0550431@gmail.com>
2026-06-27 21:19:34 +0800

    lets try this

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -8,7 +8,13 @@ const cors = require("cors");
 const bcrypt = require("bcrypt");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ['https://hawkkyy.github.io/TeamRocketCo/', 'http://localhost:3000'],
+  credentials: true
+}));
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
