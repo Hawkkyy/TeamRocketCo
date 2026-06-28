3ba89a4cf9c19e61506cfcb49240c387448467fd
Hawkyy <tr0550431@gmail.com>
2026-06-28 22:28:46 +0800

    Update server.js

--- NodeJs/server.js
+++ NodeJs/server.js
@@ -14,10 +14,18 @@ const app = express();
 // 2. MIDDLEWARE HOOKS
 app.use(cors({
     origin: function (origin, callback) {
-        const allowedOrigins = ['https://hawkkyy.github.io', 'http://localhost:3000', 'http://127.0.0.1:5500'];
+        const allowedOrigins = [
+            'https://hawkkyy.github.io', 
+            'http://localhost:3000', 
+            'http://127.0.0.1:5500',
+            'http://localhost:5500' 
+        ];
+        
         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
             callback(null, true);
         } else {
+            // ADDED: This log will print the exact hidden domain causing the failure to your Render terminal
+            console.error("CORS Blocked Origin Authority:", origin);
             callback(new Error('Not allowed by CORS'));
         }
     },
